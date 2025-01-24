dc387cd874c9e65bc64f3f2dff4dfd2305e366c5
PC Salón 120 <cs113invop@gmail.com>
2025-01-24 13:58:28 -0600

    Viernes 24

--- Semana 1/Program1.py
+++ Semana 1/Program1.py
@@ -2,7 +2,7 @@
 
 #Respuesta : a)
 
-
+problema1 = "a)"
 
 
 print("Hola Mundo desde Mi PC para GitHub")
---
+++ Semana 1/TrabajoSalon1.py
@@ -0,0 +1,57 @@
+#print("Actividades salón ")
+
+v1 = 10000
+
+v1 -= 2
+
+if v1 <= 0:
+    if v1 >= 99:
+        print("Dentro del if")
+        print("Aun dentro del if")
+
+#print("Ya me salí")
+
+#nombre = input()
+
+#print("Tu nombre es: ",nombre)
+
+#Casting -> Casteo
+#numero = int(float(input("Ingrese un numero: ")))
+
+#print(numero)
+'''
+edad = -1
+anioactual = 2025
+anionacimiento = int(input("Ingrese el año de nacimiento: "))
+
+edad = anioactual - anionacimiento
+
+print("Tienes", edad, "años sin bañarte")
+print("Tienes %.32f años sin bañarte"% edad)
+'''
+
+#Acividad 1:
+
+print("Hola Mundo")
+
+#Actividad 2:
+from fractions import Fraction
+from decimal import Decimal
+
+a = 1 #int(input("Ingrese el primer número: "))
+b = 5 #int(input("Ingrese el segundo número: "))
+
+print("adición = ",a + b)
+print("sustracción = ",a - b)
+print("multipliación = ",a * b)
+print("división = ", Fraction(Decimal(a) / Decimal(b)))
+
+#Actividad 3
+
+num = 5
+
+
+
+
+
+
