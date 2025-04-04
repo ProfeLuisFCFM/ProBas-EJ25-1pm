6be5b2170fb539035f21f5c750537e262ffcc495
HP240G <cs113invop@gmail.com>
2025-04-04 00:35:14 -0600

    Sync semana 11

--- Semana 11/Archivos.py
+++ Semana 11/Archivos.py
@@ -1,12 +1,16 @@
 #Elegante
 
-path = "C:\\Users\\AULA 120\\Documents\\Github\\ProBas-EJ25\\Semana 10\\"
+path = {
+        'FCFM': "C:\\Users\\AULA 120\\Documents\\Github\\ProBas-EJ25\\Semana 10\\",
+        'Personal': "C:\\Users\\luisG\\OneDrive\\Documentos\\GithubFCFM\\ProBas-EJ25-1pm\\Semana 10\\"  
+        }
+
 
 nombre = "divideyvenceras.py"
 
 nombre2 = "recursivo.py"
 
-file = open(path+nombre,"r")
+file = open(path['Personal']+nombre,"r")
 
 for line in file:
     print(line)
@@ -16,6 +20,13 @@ file.close()
 
 #No Elegante
 
-with open(path+nombre2,"r",encoding="utf8") as fichero2:
+with open(path['Personal']+nombre2,"r",encoding="utf8") as fichero2:
     for linea in fichero2.readlines():
         print(linea)
+
+edad = 18
+
+with open("escritura.txt", "w", encoding="utf8") as fich:
+    fich.write("hola\tmundo\tdesde monterrey a 45°C\n")
+
+    print(f"La mayoría de edad es a los {edad} años.", file=fich)
